8e2b1f0aa2c9ce7317392ed9a279d125cd4cc10e
diegoFm10 <diego479@gmail.com>
2022-01-11 16:58:03 -0500

    DbCOntext configuration

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] //Data Annotation Validator to declare "Id" as a primary key and identity column to Entity Framework
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;//Al igualar la variable se le agrega un valor predeterminado
+    }
+}
